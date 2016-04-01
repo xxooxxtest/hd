a6fd42707c3ad2e1b50249592a472fe0c7ecf5f5
liuyanchen <liuyanchen@nationsky.com>
2016-04-02 00:49:42 +0800

    project init

---
+++ HdPlugin/src/com/first/hd/host/FindFragmentExt.java
@@ -0,0 +1,17 @@
+package com.first.hd.host;
+
+import android.app.Fragment;
+import android.view.View;
+
+import com.example.service1.R;
+
+public class FindFragmentExt {
+
+	public  static int getLayoutId(){
+		return R.layout.zz_test_view;
+	}
+	
+	public static void initView(View view){
+	}
+	
+}
---
+++ HdPlugin/src/com/first/hd/plugin/MainActivity.java
@@ -0,0 +1,20 @@
+package com.first.hd.plugin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		return true;
+	}
+
+}
